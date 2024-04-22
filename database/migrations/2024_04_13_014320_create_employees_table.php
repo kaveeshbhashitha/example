ac0a3ea7a9a6d3b120281a0c942caebf5fa5ebb2
kaveeshbhashitha <kaveeshbhashitha@gmail.com>
2024-04-22 12:14:18 +0530

    new commite for migration

--- database/migrations/2024_04_13_014320_create_employees_table.php
+++ database/migrations/2024_04_13_014320_create_employees_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('address');
             $table->string('mobile');
+            $table->string('newcolumn');
             $table->string('photo', 300);
             $table->timestamps();
             
